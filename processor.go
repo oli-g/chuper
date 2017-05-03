3ce6110752484b35a028532c3ba47dbfba436e25
sluceno <sluceno.a@gmail.com>
2017-05-03 11:42:51 +0200

    Return raw response body apart of the goquery document.

--- processor.go
+++ processor.go
@@ -1,15 +1,13 @@
 package chuper
 
-import (
-	"github.com/PuerkitoBio/goquery"
-)
+import "github.com/PuerkitoBio/goquery"
 
 type Processor interface {
-	Process(Context, *goquery.Document) bool
+	Process(Context, []byte, *goquery.Document) bool
 }
 
-type ProcessorFunc func(Context, *goquery.Document) bool
+type ProcessorFunc func(Context, []byte, *goquery.Document) bool
 
-func (p ProcessorFunc) Process(ctx Context, doc *goquery.Document) bool {
-	return p(ctx, doc)
+func (p ProcessorFunc) Process(ctx Context, body []byte, doc *goquery.Document) bool {
+	return p(ctx, body, doc)
 }
